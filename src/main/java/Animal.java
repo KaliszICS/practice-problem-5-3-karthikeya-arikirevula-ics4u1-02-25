1317483b71608db8925bde05ab23293d93f36024
Karthikeya Arikirevula <karthikeya.arikirevula@student.tdsb.on.ca>
2025-05-29 22:32:32 +0000

    done

---
+++ src/main/java/Animal.java
@@ -0,0 +1,37 @@
+abstract class Animal {
+    protected String name;
+    protected int age;
+    protected String type;
+    protected int position;
+
+    public Animal(String name, int age, String type, int position) {
+        this.name = name;
+        this.age = age;
+        this.type = type;
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    abstract String animalSound();
+
+    abstract void move();
+
+    public String sleep() {
+        return "Zzz";
+    }
+}
